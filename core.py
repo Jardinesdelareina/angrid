743c6faf8b09b5841f5766bdce111b63808ec0ac
jardinesdelareina <jardinesdelareina@mail.ru>
2023-07-12 03:04:32 +0300

    размещение ордеров

--- core.py
+++ core.py
@@ -1,4 +1,5 @@
 import time
+import json
 import asyncio
 import pandas as pd
 from binance import BinanceSocketManager
@@ -7,10 +8,12 @@ from config import CLIENT, ENGINE
 from utils import round_list
 
 
-class BinanceAPI:
+class Angrid:
 
-    def __init__(self, symbol):
+    def __init__(self, symbol: str, price_step_percent: float = 1.0, depth_grid: int = 10):
         self.symbol = symbol
+        self.price_step_percent = price_step_percent
+        self.depth_grid = depth_grid
 
 
     def create_frame(self, stream):
@@ -26,8 +29,43 @@ class BinanceAPI:
             df.to_sql(name=f'{self.db_ticker}', con=ENGINE, if_exists='append', index=False)
         except KeyError:
             pass
-
     
+
+    def place_buy_limit(self, price: float):
+        asset_balance = CLIENT.get_asset_balance('USDT')
+        balance_free = float(asset_balance.get('free'))
+        qnty = balance_free / 10
+        order_buy_limit = CLIENT.order_limit_buy(
+            symbol=self.symbol,
+            quantity=qnty,
+            price=price
+        )
+        print(json.dumps(order_buy_limit, indent=4, sort_keys=True))
+
+
+    def plase_sell_limit(self, price: float):
+        ticker = self.symbol.replace('')
+        asset_balance = CLIENT.get_asset_balance(ticker)
+        balance_free = float(asset_balance.get('free'))
+        qnty = balance_free / 10
+        order_sell_limit = CLIENT.order_limit_buy(
+            symbol=self.symbol,
+            quantity=qnty,
+            price=price
+        )
+        print(json.dumps(order_sell_limit, indent=4, sort_keys=True))
+
+
+    def create_grid(self, start_price: float):
+        for i in range(self.depth_grid):
+            buy_price = start_price * (1 - self.price_step_percent/100 * (i+1))
+            self.place_buy_limit(buy_price)
+
+        for i in range(self.depth_grid):
+            sell_price = start_price * (1 + self.price_step_percent/100 * (i+1))
+            self.place_sell_limit(sell_price)
+
+
     async def socket_stream(self):
         bm = BinanceSocketManager(client=CLIENT)
         ts = bm.symbol_ticker_socket(self.symbol)
@@ -42,3 +80,4 @@ class BinanceAPI:
                         time.sleep(5)
                         self.create_frame(res)
                 await asyncio.sleep(0)
+                
--- main.py
+++ main.py
@@ -1,20 +1,20 @@
 import asyncio
 from concurrent.futures import ThreadPoolExecutor
-from core import BinanceAPI
+from core import Angrid
 from utils import symbol_list
 
 
 def start_single_bot(symbol):
     loop = asyncio.new_event_loop()
     asyncio.set_event_loop(loop)
-    asyncio.run(BinanceAPI(symbol).socket_stream())
+    asyncio.run(Angrid(symbol).socket_stream())
 
 
 def start_all_bots():
-    bots = [BinanceAPI(symbol) for symbol in symbol_list]
+    bots = [Angrid(symbol) for symbol in symbol_list]
     with ThreadPoolExecutor() as executor:
         return executor.map(asyncio.run, [bot.socket_stream() for bot in bots])
 
 
-#start_single_bot('BTCUSDT')
+start_single_bot('XRPUSDT')
 #start_all_bots()
