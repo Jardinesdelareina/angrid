b389bcc0528f419b96b708b743ac6072385709e9
jardinesdelareina <jardinesdelareina@mail.ru>
2023-07-16 11:03:47 +0300

    Создание двух таблиц в базе данных

--- core.py
+++ core.py
@@ -18,6 +18,9 @@ class Angrid:
         SELL-ордера, что автоматически переквалифицирует стратегию в `buy and hold`.
     """
 
+    INIT = False    # Инициализация алгоритма
+
+
     def __init__(self, symbol: str, price_step_percent: float = 1.0, depth_grid: int = 5):
         """ symbol (str): Криптовалютный тикер
             price_step_percent (float): Дистанция между лимитными ордерами в сетке ордеров (в процентах)
@@ -55,19 +58,18 @@ class Angrid:
             df_order = df_order.rename(columns={
                 'orderId': 'order_id',
                 'clientOrderId': 'client_order_id',
-                'transactTime': 'transact_time',
                 'origQty': 'orig_qty',
                 'executedQty': 'executed_qty',
+                'cummulativeQuoteQty': 'cummulative_quote_qty',
                 'timeInForce': 'time_in_force',
+                'isWorking': 'is_working'
             })
-            df_order.transact_time = pd.Series(pd.to_datetime(df_order.transact_time, unit='ms', utc=True))\
+            df_order.time = pd.Series(pd.to_datetime(df_order.time, unit='ms', utc=True))\
                 .dt.strftime('%Y-%m-%d %H:%M:%S')
             df_order.price = round(df_order.price.astype(float), round_list[f'{self.symbol}'])
             df_order.orig_qty = df_order.orig_qty.astype(float)
             df_order.execute_qty = df_order.execute_qty.astype(float)
-            db_ticker_order = df_order.symbol.str.lower().iloc[0]
-            df_order.to_sql(name=f'{db_ticker_order}', con=ENGINE, if_exists='append', index=False)
-            print(f'{df_order.symbol}: {df_order.type} {df_order.side} {df_order.price} {df_order.status}')
+            df_order.to_sql(name='orders', con=ENGINE, if_exists='append', index=False)
         except bae:
             message = 'Для размещения ордера недостаточно средств'
             print(message)
@@ -90,12 +92,16 @@ class Angrid:
             buy_list.append(buy_price)
             if not DEBUG:
                 self.place_order('BUY_LIMIT', buy_price)
+            else:
+                print(f'{self.symbol} BUY LIMIT {buy_price}')
 
         for i in range(self.depth_grid):
             sell_price = float(start_price * (1 + self.price_step_percent/100 * (i+1)))
             sell_list.append(sell_price)
             if not DEBUG:
                 self.place_order('SELL_LIMIT', sell_price)
+            else:
+                print(f'{self.symbol} SELL LIMIT {sell_price}')
 
         message = f'BUY: {[buy for buy in buy_list]} \n SELL: {[sell for sell in sell_list]}'
         send_message(message)        
@@ -111,9 +117,7 @@ class Angrid:
             df.time = pd.Series(pd.to_datetime(df.time, unit='ms', utc=True)).dt.strftime('%Y-%m-%d %H:%M:%S')
             for column in ['bid', 'ask']:
                 df[column] = round(df[column].astype(float), round_list[f'{self.symbol}'])
-            #print(f'{df.symbol}: {df.time} {df.bid} {df.ask}')
-            db_ticker = df.symbol.str.lower().iloc[0]
-            df.to_sql(name=f'{db_ticker}', con=ENGINE, if_exists='append', index=False)
+            df.to_sql(name='market_stream', con=ENGINE, if_exists='append', index=False)
         except KeyError:
             pass
 
--- init.sql
+++ init.sql
@@ -1,2 +1,60 @@
 DROP DATABASE IF EXISTS angrid;
 CREATE DATABASE angrid;
+
+/* Поток рыночных данных */
+CREATE TABLE IF NOT EXISTS market_stream
+(
+    time DATETIME UNIQUE NOT NULL,
+    symbol VARCHAR(10) NOT NULL,
+    bid FLOAT NOT NULL,
+    ask FLOAT NOT NULL
+);
+
+/* 
+Лимитный ордер:
+symbol - символ торговой пары,
+order_id - идентификатор ордера,
+client_order_id - пользовательский идентификатор ордера,
+price - цена ордера,
+orig_qty - начальное количество актива,
+executed_qty - количество актива, которое уже исполнено,
+cummulative_quote_qty - суммарное количество котируемой валюты,
+status - статус ордера,
+    Возможные статусы лимитных ордеров:
+        NEW: Ордер создан и ожидает выполнения
+        PARTIALLY_FILLED: Ордер частично выполнен
+        FILLED: Ордер полностью выполнен
+        CANCELED: Ордер отменен пользователем
+        PENDING_CANCEL: Ордер находится в процессе отмены
+        REJECTED: Ордер отклонен
+        EXPIRED: Ордер истек (не был выполнен в определенный срок)
+time_in_force - тип срока действия ордера, по-умолчанию "GTC" (Good Till Cancelled),
+type - тип ордера, по-умолчанию "LIMIT" (лимитный),
+side - направление ордера, "BUY" (покупка) или "SELL" (продажа),
+time - время создания ордера,
+is_working - флаг, указывающий, активен ли ордер
+*/
+CREATE TABLE IF NOT EXISTS orders
+(
+    symbol VARCHAR(10) NOT NULL,
+    order_id INT UNIQUE NOT NULL,
+    client_order_id VARCHAR(100) UNIQUE NOT NULL,
+    price FLOAT NOT NULL,
+    orig_qty FLOAT NOT NULL,
+    executed_qty FLOAT NOT NULL,
+    cummulative_quote_qty FLOAT NOT NULL,
+    status VARCHAR CHECK (status IN (
+        'NEW', 
+        'PARTIALLY_FILLED',
+        'FILLED',
+        'CANCELED',
+        'PENDING_CANCEL',
+        'REJECTED',
+        'EXPIRED'
+    )),
+    time_in_force VARCHAR(3) DEFAULT 'GTC',
+    type VARCHAR(5) DEFAULT 'LIMIT',
+    side VARCHAR CHECK (side IN ('BUY', 'SELL')),
+    time DATETIME UNIQUE NOT NULL,
+    is_working BOLL NOT NULL
+) 
